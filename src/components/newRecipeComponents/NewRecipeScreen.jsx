af634dbcb8d3dd64430c23000a0b4bf36a25225b
Jessica Swenson <jessicamswens@gmail.com>
2023-03-05 13:30:01 -0700

    added new page

--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -21,28 +21,28 @@ const NewRecipeScreen = () => {
   return (
     <section>
       <h1>Tell us about your Recipe!</h1>
-      <Formik initialValues={initialValues} onSubmit={onSubmit} >
-        {({ values, handleChange, handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <input placeholder="recipe name" />
-            <input placeholder="recipe image" />
-            <input placeholder="recipe prep time" />
-            <input placeholder="recipe cook time" />
-            <input placeholder="recipe serves" />
-            <input placeholder="recipe ingredient" />
-            <input placeholder="recipe quantity" />
-            <textarea placeholder="instructions" />
-            <div>
-              <input type="radio" id="cook" name="cook" value="cook" />
-              <label for="cook">Cook</label>
-              <input type="radio" id="bake" name="bake" value="bake" />
-              <label for="bake">Bake</label>
-              <input type="radio" id="drink" name="drink" value="drink" />
-              <label for="drink">Drink</label>
-            </div>
-          </form>
-        )}
-        <Formik />
+      {/* <Formik initialValues={initialValues} onSubmit={onSubmit} > */}
+      {({ values, handleChange, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <input placeholder="recipe name" />
+          <input placeholder="recipe image" />
+          <input placeholder="recipe prep time" />
+          <input placeholder="recipe cook time" />
+          <input placeholder="recipe serves" />
+          <input placeholder="recipe ingredient" />
+          <input placeholder="recipe quantity" />
+          <textarea placeholder="instructions" />
+          <div>
+            <input type="radio" id="cook" name="cook" value="cook" />
+            <label for="cook">Cook</label>
+            <input type="radio" id="bake" name="bake" value="bake" />
+            <label for="bake">Bake</label>
+            <input type="radio" id="drink" name="drink" value="drink" />
+            <label for="drink">Drink</label>
+          </div>
+        </form>
+      )}
+      {/* <Formik /> */}
     </section>
   );
 };
